d4ed684df77d42856127866262d9d0694121ae90
LucianoFT <lucaxpto@gmail.com>
2020-04-03 16:37:55 -0300

    adicionando barra de navegacao

---
+++ src/app/navbar/navbar.componnent.ts
@@ -0,0 +1,10 @@
+import {Component, ComponentFactory} from '@angular/core';
+
+@Component({
+  selector: 'navbar',
+  templateUrl: './navbar.component.html'  
+})
+
+export class NavbarComponent{
+
+}
