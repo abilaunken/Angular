bd7305edd162a6814f7b4536953e42783b8291a1
LucianoFT <lucaxpto@gmail.com>
2020-04-07 12:29:34 -0300

    Adicionando o tasks compontent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,15 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.componnent';
+import { NavbarComponent } from './navbar/navbar.component';
+
+import { TasksComponent } from './tasks/tasks.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule,
