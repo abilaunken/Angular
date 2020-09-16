d86fa830e9d7621389dba6ac1e07e3fb91ec1b0a
LucianoFT <lucaxpto@gmail.com>
2020-09-16 10:12:20 -0300

    adicionando navageacao entre o dashboard component e o taskdetail

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from "@angular/core";
 import { Task } from "../tasks/shared/task.model";
 import { TaskService } from "../tasks/shared/task.service";
 
+
 @Component({
     selector: 'dashboard',
     templateUrl: './dashboard.component.html',
